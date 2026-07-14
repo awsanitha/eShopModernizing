0e123b438d0b35724aa290c9570888b1fc5dffc5
Version Upgrade <n/a>
2026-07-14 11:13:11 +0000

    Upgrade to .NET 10 on Iteration 7

--- eShopLegacyMVCSolution/src/eShopLegacyMVC/Modules/ApplicationModule.cs
+++ eShopLegacyMVCSolution/src/eShopLegacyMVC/Modules/ApplicationModule.cs
@@ -1,4 +1,4 @@
-﻿using Autofac;
+using Autofac;
 using eShopLegacyMVC.Models;
 using eShopLegacyMVC.Models.Infrastructure;
 using eShopLegacyMVC.Services;
@@ -7,12 +7,13 @@ namespace eShopLegacyMVC.Modules
 {
     public class ApplicationModule : Module
     {
-        private bool useMockData;
+        private readonly bool useMockData;
 
         public ApplicationModule(bool useMockData)
         {
             this.useMockData = useMockData;
         }
+
         protected override void Load(ContainerBuilder builder)
         {
             if (this.useMockData)
@@ -28,14 +29,11 @@ namespace eShopLegacyMVC.Modules
                     .InstancePerLifetimeScope();
             }
 
-            builder.RegisterType<CatalogDBContext>()
-                .InstancePerLifetimeScope();
+            builder.RegisterType<CatalogItemHiLoGenerator>()
+                .SingleInstance();
 
             builder.RegisterType<CatalogDBInitializer>()
                 .InstancePerLifetimeScope();
-
-            builder.RegisterType<CatalogItemHiLoGenerator>()
-                .SingleInstance();
         }
     }
 }
