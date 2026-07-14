65afb7a67c8321b3a76f9ffa66dc5c8aaf273f81
Version Upgrade <n/a>
2026-07-14 05:08:45 +0000

    Upgrade to .NET 10 on Iteration 2

--- eShopLegacyMVCSolution/src/eShopLegacyMVC/Controllers/PicController.cs
+++ eShopLegacyMVCSolution/src/eShopLegacyMVC/Controllers/PicController.cs
@@ -1,25 +1,27 @@
-﻿using eShopLegacyMVC.Services;
+using eShopLegacyMVC.Services;
 using log4net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using System.IO;
-using System.Net;
-using System.Web.Mvc;
 
 namespace eShopLegacyMVC.Controllers
 {
     public class PicController : Controller
     {
-        private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod()!.DeclaringType);
 
         public const string GetPicRouteName = "GetPicRouteTemplate";
 
-        private ICatalogService service;
+        private readonly ICatalogService service;
+        private readonly IWebHostEnvironment env;
 
-        public PicController(ICatalogService service)
+        public PicController(ICatalogService service, IWebHostEnvironment env)
         {
             this.service = service;
+            this.env = env;
         }
 
-        // GET: Pic/5.png
+        // GET: items/{catalogItemId}/pic
         [HttpGet]
         [Route("items/{catalogItemId:int}/pic", Name = GetPicRouteName)]
         public ActionResult Index(int catalogItemId)
@@ -28,17 +30,22 @@ namespace eShopLegacyMVC.Controllers
 
             if (catalogItemId <= 0)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return BadRequest();
             }
 
             var item = service.FindCatalogItem(catalogItemId);
 
             if (item != null)
             {
-                var webRoot = Server.MapPath("~/Pics");
-                var path = Path.Combine(webRoot, item.PictureFileName);
+                var picsPath = Path.Combine(env.ContentRootPath, "Pics");
+                var path = Path.Combine(picsPath, item.PictureFileName ?? "dummy.png");
 
-                string imageFileExtension = Path.GetExtension(item.PictureFileName);
+                if (!System.IO.File.Exists(path))
+                {
+                    return NotFound();
+                }
+
+                string imageFileExtension = Path.GetExtension(item.PictureFileName ?? ".png");
                 string mimetype = GetImageMimeTypeFromImageFileExtension(imageFileExtension);
 
                 var buffer = System.IO.File.ReadAllBytes(path);
@@ -46,46 +53,23 @@ namespace eShopLegacyMVC.Controllers
                 return File(buffer, mimetype);
             }
 
-            return HttpNotFound();
+            return NotFound();
         }
 
-        private string GetImageMimeTypeFromImageFileExtension(string extension)
+        private static string GetImageMimeTypeFromImageFileExtension(string extension)
         {
-            string mimetype;
-
-            switch (extension)
+            return extension switch
             {
-                case ".png":
-                    mimetype = "image/png";
-                    break;
-                case ".gif":
-                    mimetype = "image/gif";
-                    break;
-                case ".jpg":
-                case ".jpeg":
-                    mimetype = "image/jpeg";
-                    break;
-                case ".bmp":
-                    mimetype = "image/bmp";
-                    break;
-                case ".tiff":
-                    mimetype = "image/tiff";
-                    break;
-                case ".wmf":
-                    mimetype = "image/wmf";
-                    break;
-                case ".jp2":
-                    mimetype = "image/jp2";
-                    break;
-                case ".svg":
-                    mimetype = "image/svg+xml";
-                    break;
-                default:
-                    mimetype = "application/octet-stream";
-                    break;
-            }
-
-            return mimetype;
+                ".png" => "image/png",
+                ".gif" => "image/gif",
+                ".jpg" or ".jpeg" => "image/jpeg",
+                ".bmp" => "image/bmp",
+                ".tiff" => "image/tiff",
+                ".wmf" => "image/wmf",
+                ".jp2" => "image/jp2",
+                ".svg" => "image/svg+xml",
+                _ => "application/octet-stream"
+            };
         }
     }
 }
--- eShopLegacyMVCSolution/src/eShopLegacyMVC/Modules/ApplicationModule.cs
+++ eShopLegacyMVCSolution/src/eShopLegacyMVC/Modules/ApplicationModule.cs
@@ -1,4 +1,4 @@
-﻿using Autofac;
+using Autofac;
 using eShopLegacyMVC.Models;
 using eShopLegacyMVC.Models.Infrastructure;
 using eShopLegacyMVC.Services;
@@ -7,12 +7,13 @@ namespace eShopLegacyMVC.Modules
 {
     public class ApplicationModule : Module
     {
-        private bool useMockData;
+        private readonly bool useMockData;
 
         public ApplicationModule(bool useMockData)
         {
             this.useMockData = useMockData;
         }
+
         protected override void Load(ContainerBuilder builder)
         {
             if (this.useMockData)
@@ -26,16 +27,10 @@ namespace eShopLegacyMVC.Modules
                 builder.RegisterType<CatalogService>()
                     .As<ICatalogService>()
                     .InstancePerLifetimeScope();
+
+                builder.RegisterType<CatalogItemHiLoGenerator>()
+                    .SingleInstance();
             }
-
-            builder.RegisterType<CatalogDBContext>()
-                .InstancePerLifetimeScope();
-
-            builder.RegisterType<CatalogDBInitializer>()
-                .InstancePerLifetimeScope();
-
-            builder.RegisterType<CatalogItemHiLoGenerator>()
-                .SingleInstance();
         }
     }
 }
